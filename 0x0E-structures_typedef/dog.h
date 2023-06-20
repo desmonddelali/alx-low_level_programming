cca1c4f0d66ead5618bb771683fa8c3759febc87
desmonddelali <datakpla@gmail.com>
2023-06-19 19:00:08 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
+
 
 /**
   * struct dog - Dog attributes
@@ -17,7 +18,7 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for dog structure
+ * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 
@@ -25,4 +26,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+
 #endif
